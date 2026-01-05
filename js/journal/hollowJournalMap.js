8879223edc77c4b094d0eb873698f90eaa7fdd69
novice <289836737@qq.com>
2026-01-05 21:33:54 +0800

    update: 初始化了猎人日志解析内容，但是字段尚未填写...

---
+++ js/journal/hollowJournalMap.js
@@ -0,0 +1,10 @@
+window.JOURNAL_MAPS = window.JOURNAL_MAPS || {};
+window.JOURNAL_MAPS.hollow = {
+    entries: [
+        { name: "古蜥蜴", killedKey: "killedGruzMother", killsKey: "killsGruzMother", requiredKills: 1 },
+        { name: "狂虫", killedKey: "killedInfectedBalloon", killsKey: "killsInfectedBalloon", requiredKills: 1 },
+        { name: "螳螂幼体", killedKey: "killedMantisCrawler", killsKey: "killsMantisCrawler", requiredKills: 1 },
+        { name: "哀鸣之蝇", killedKey: "killedVengefly", killsKey: "killsVengefly", requiredKills: 1 },
+        { name: "蘑菇炮手", killedKey: "killedMushroomTurret", killsKey: "killsMushroomTurret", requiredKills: 1 }
+    ]
+};
---
+++ js/journal/silksongJournalMap.js
@@ -0,0 +1,8 @@
+window.JOURNAL_MAPS = window.JOURNAL_MAPS || {};
+window.JOURNAL_MAPS.silksong = {
+    entries: [
+        { name: "丝蜂守卫", killedKey: "killedHiveGuard", killsKey: "killsHiveGuard", requiredKills: 1 },
+        { name: "织丝弓手", killedKey: "killedWeaverArcher", killsKey: "killsWeaverArcher", requiredKills: 1 },
+        { name: "深巢爬行者", killedKey: "killedDeepnestCreeper", killsKey: "killsDeepnestCreeper", requiredKills: 1 }
+    ]
+};
