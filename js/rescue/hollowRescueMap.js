a372bd4908237d6d420ee9d23f9a19395c3d0282
novice <289836737@qq.com>
2026-01-16 15:22:35 +0800

    更新: 新增幼虫和跳蚤的收集解析和地点标识

---
+++ js/rescue/hollowRescueMap.js
@@ -0,0 +1,314 @@
+window.RESCUE_MAPS = window.RESCUE_MAPS || {};
+window.RESCUE_MAPS.hollow = {
+    entries: [
+        {
+            scene: "Crossroads_48",
+            name: "遗忘十字路",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "黑卵圣殿右侧，需要打败/绕过躯壳守卫",
+            locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/4/44/Grub_Forgotten_Crossroads_Location_1.png"
+        },
+        {
+            scene: "Crossroads_35",
+            name: "遗忘十字路",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/8/8f/Grub_Forgotten_Crossroads_Location_2.png"
+        },
+        {
+            scene: "Crossroads_03",
+            name: "遗忘十字路",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "需要打破可破坏的墙壁",
+            locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/6/60/Grub_Forgotten_Crossroads_Location_3.png"
+        },
+        {
+            scene: "Crossroads_31",
+            name: "遗忘十字路",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "需要下批跳过刺",
+            locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/f/fd/Grub_Forgotten_Crossroads_Location_4.png"
+        },
+        {
+            scene: "Crossroads_05",
+            name: "遗忘十字路",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "需要蛾翼披风或对刺下劈",
+            locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/a/a7/Grub_Forgotten_Crossroads_Location_5.png"
+        },
+        {
+            scene: "Fungus1_06",
+            name: "苍绿之径",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/e/ea/Grub_Greenpath_Location_1.png"
+        },
+        {
+            scene: "Fungus1_07",
+            name: "苍绿之径",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/5/53/Grub_Greenpath_Location_2.png"
+        },
+        {
+            scene: "Fungus1_21",
+            name: "苍绿之径",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "需要打败苔藓骑士",
+            locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/4/4d/Grub_Greenpath_Location_3.png"
+        },   
+        {
+            scene: "Fungus1_13",
+            name: "苍绿之径",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "需要蛾翼披风",
+            locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Grub_Greenpath_Location_5.png"
+        },
+        {
+            scene: "Ruins1_05",
+            name: "Ruins1_05",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Ruins1_32",
+            name: "Ruins1_32",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Waterways_04",
+            name: "Waterways_04",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Fungus2_20",
+            name: "Fungus2_20",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Fungus1_28",
+            name: "Fungus1_28",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Mines_03",
+            name: "Mines_03",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Mines_31",
+            name: "Mines_31",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Mines_16",
+            name: "Mines_16",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Mines_04",
+            name: "Mines_04",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "RestingGrounds_10",
+            name: "RestingGrounds_10",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Ruins_House_01",
+            name: "Ruins_House_01",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Abyss_17",
+            name: "Abyss_17",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Mines_35",
+            name: "Mines_35",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Abyss_19",
+            name: "Abyss_19",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Mines_24",
+            name: "Mines_24",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Deepnest_31",
+            name: "Deepnest_31",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Deepnest_39",
+            name: "Deepnest_39",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Deepnest_03",
+            name: "Deepnest_03",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Deepnest_Spider_Town",
+            name: "Deepnest_Spider_Town",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Fungus2_18",
+            name: "Fungus2_18",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Waterways_13",
+            name: "Waterways_13",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Ruins2_03",
+            name: "Ruins2_03",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Deepnest_East_11",
+            name: "Deepnest_East_11",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Ruins2_07",
+            name: "Ruins2_07",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Waterways_14",
+            name: "Waterways_14",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Fungus3_47",
+            name: "Fungus3_47",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Fungus3_48",
+            name: "Fungus3_48",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Fungus3_10",
+            name: "Fungus3_10",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Fungus3_22",
+            name: "Fungus3_22",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Ruins2_11",
+            name: "Ruins2_11",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Hive_03",
+            name: "Hive_03",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Hive_04",
+            name: "Hive_04",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Deepnest_East_14",
+            name: "Deepnest_East_14",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Deepnest_36",
+            name: "Deepnest_36",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            scene: "Mines_19",
+            name: "Mines_19",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/0/0c/Grub.png",
+            desc: "",
+            locationImage: ""
+        },
+        
+    ]
+};
---
+++ js/rescue/silksongRescueMap.js
@@ -0,0 +1,194 @@
+window.RESCUE_MAPS = window.RESCUE_MAPS || {};
+window.RESCUE_MAPS.silksong = {
+    entries: [
+        {
+            key: "SavedFlea_Bone_06",
+            name: "Bone_06",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Dock_16",
+            name: "Dock_16",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Bone_East_05",
+            name: "Bone_East_05",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Bone_East_17b",
+            name: "Bone_East_17b",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Ant_03",
+            name: "Ant_03",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Greymoor_15b",
+            name: "Greymoor_15b",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Greymoor_06",
+            name: "Greymoor_06",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Shellwood_03",
+            name: "Shellwood_03",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Bone_East_10_Church",
+            name: "Bone_East_10_Church",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Coral_35",
+            name: "Coral_35",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Dust_12",
+            name: "Dust_12",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Dust_09",
+            name: "Dust_09",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Belltown_04",
+            name: "Belltown_04",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Crawl_06",
+            name: "Crawl_06",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Slab_Cell",
+            name: "Slab_Cell",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Shadow_28",
+            name: "Shadow_28",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Dock_03d",
+            name: "Dock_03d",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Under_23",
+            name: "Under_23",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Shadow_10",
+            name: "Shadow_10",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Song_14",
+            name: "Song_14",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Coral_24",
+            name: "Coral_24",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Peak_05c",
+            name: "Peak_05c",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Library_09",
+            name: "Library_09",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Song_11",
+            name: "Song_11",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Library_01",
+            name: "Library_01",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Under_21",
+            name: "Under_21",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        },
+        {
+            key: "SavedFlea_Slab_06",
+            name: "Slab_06",
+            icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
+            desc: "",
+            locationImage: ""
+        }
+    ]
+};
