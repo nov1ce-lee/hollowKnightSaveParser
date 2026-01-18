05b0b50ea284840bf787659945faac4989207263
novice <289836737@qq.com>
2026-01-18 16:45:15 +0800

    update: 部分跳蚤描述

--- js/rescue/silksongRescueMap.js
+++ js/rescue/silksongRescueMap.js
@@ -12,35 +12,35 @@ window.RESCUE_MAPS.silksong = {
             key: "SavedFlea_Dock_16",
             name: "2-深坞",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "在深坞钟道兽车站左侧一堵可破坏的墙后面",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/9/94/Flea_Deep_Docks_Location_1.png"
         },
         {
             key: "SavedFlea_Bone_East_05",
             name: "3-深坞",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "需要疾风步",
+            desc: "需要疾风步，从获得疾风步的位置向左跑，然后击打房间右上角的拉杆",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/f/f0/Flea_Deep_Docks_Location_2.png"
         },
         {
             key: "SavedFlea_Dock_03d",
             name: "4-深坞",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "需要飞针冲刺",
+            desc: "需要飞针冲刺，可以使用飞针抓住下方房间中央的圆环来到达上层区域",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/7/71/Flea_Deep_Docks_Location_3.png"
         },
         {
             key: "SavedFlea_Ant_03",
             name: "5-猎者小径",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "需要击败猎者小径入口的斯卡尔禁卫",
+            desc: "需要击败猎者小径入口的斯卡尔禁卫，经过一段小小的平台跳跃挑战后即可到达",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/c/c0/Flea_Hunter%27s_March_Location_1.png"
         },
         {
             key: "SavedFlea_Bone_East_17b",
             name: "6-远野",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "跳蚤被关在一个笼子里，笼子前面有陷阱笼",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/8/81/Flea_Far_Fields_Location_1.png"
         },
         {
@@ -54,21 +54,21 @@ window.RESCUE_MAPS.silksong = {
             key: "SavedFlea_Crawl_06",
             name: "8-沙噬虫道",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "跳蚤被一只阿克尼叼着，打败它就能救出跳蚤",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/b/bb/Flea_Wormways_Location_1.png"
         },
         {
             key: "SavedFlea_Greymoor_15b",
             name: "9-灰沼",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "需要完成腐囊虫泽的遭遇战，打开气球机关后才可到达",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/7/75/Flea_Greymoor_Location_1.png"
         },
         {
             key: "SavedFlea_Greymoor_06",
             name: "10-灰沼",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "灰沼左侧塔楼的顶端，需要蛛攀术",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/4/4f/Flea_Greymoor_Location_2.png"
         },
         {
@@ -82,35 +82,35 @@ window.RESCUE_MAPS.silksong = {
             key: "SavedFlea_Belltown_04",
             name: "12-钟心镇",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "需要打败碎裂者修姊，需要蛛攀术",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/6/69/Flea_Bellhart_Location_1.png"
         },
         {
             key: "SavedFlea_Shellwood_03",
             name: "13-甲木林",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "位于一些有固定敌人的平台下方",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/c/c4/Flea_Shellwood_Location_1.png"
         },
         {
             key: "SavedFlea_Coral_35",
             name: "14-蚀阶",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "需要蛛攀术",
+            desc: "位于钟道站左侧房间的顶部，需要蛛攀术",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/6/6d/Flea_Blasted_Steps_Location_1.png"
         },
         {
             key: "SavedFlea_Dust_12",
             name: "15-罪途",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "一只蟑螂在附近游荡，当靠近营救跳蚤时，它会发动攻击",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/4/4f/Flea_Sinner%27s_Road_Location_1.png"
         },
         {
             key: "SavedFlea_Shadow_28",
             name: "16-腐汁泽",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "位于一堵可破坏的墙后面",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/b/bd/Flea_Bilewater_Location_1.png"
         },
         {
@@ -131,21 +131,21 @@ window.RESCUE_MAPS.silksong = {
             key: "SavedFlea_Under_23",
             name: "19-圣堡工厂",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "需要穿过可破坏的天花板，然后经过一些简短的平台跳跃挑战",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/0/09/Flea_Underworks_Location_1.png"
         },
         {
             key: "SavedFlea_Under_21",
             name: "20-圣堡工厂",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "房间里有很多敌人",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/f/fc/Flea_Underworks_Location_2.png"
         },
         {
             key: "SavedFlea_Song_14",
             name: "21-圣咏殿",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "经过一段平台跳跃关卡后即可到达",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/7/71/Flea_Choral_Chambers_Location_1.png"
         },
         {
@@ -166,35 +166,35 @@ window.RESCUE_MAPS.silksong = {
             key: "tamedGiantFlea",
             name: "24-忆廊",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/8/82/B_Huge_Flea.png",
-            desc: "需要幻羽披风，该跳蚤为巨蚤",
+            desc: "需要幻羽披风，该跳蚤为巨蚤，需要打败他以驯服",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/c/cd/Flea_Choral_Chambers_Location_3.png"
         },
         {
             key: "SavedFlea_Slab_Cell",
             name: "25-罪石牢狱",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "一扇被雾气笼罩的门内，可以选择“进入”",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/c/c3/Flea_Slab_Location_1.png"
         },
         {
             key: "SavedFlea_Slab_06",
             name: "26-罪石牢狱",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "跳蚤位于休息长凳上方，在开口左上方的一个小房间里",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/3/30/Flea_Slab_Location_2.png"
         },
         {
             key: "SavedFlea_Peak_05c",
             name: "27-费耶山",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "跳蚤被冻在冰里，必须用针刺它才能把它弄出来",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/8/83/Flea_Mount_Fay_Location_1.png"
         },
         {
             key: "SavedFlea_Coral_24",
             name: "28-卡拉卡沙川",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "位于房间顶部，可从右侧覆盖着珊瑚的后面到达，也可以通过使用超级跳来到达",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/6/67/Flea_Sands_of_Karak_Location_1.png"
         },
         {
@@ -208,7 +208,7 @@ window.RESCUE_MAPS.silksong = {
             key: "SavedFlea_Library_01",
             name: "30-低语书库",
             icon: "https://huiji-public.huijistatic.com/hkss/uploads/d/d2/Flea.png",
-            desc: "",
+            desc: "房间顶部可通过推动位于房间左侧中间的箱子到达",
             locationImage: "https://huiji-public.huijistatic.com/hkss/uploads/d/dc/Flea_Whispering_Vaults_Location_1.png"
         },
     ]
